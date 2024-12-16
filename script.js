b02efaa20788388df3835c5d2c59dcad7bb212f1
Nathan-design-creator <anyiamchidumebi@gmail.com>
2024-12-16 08:31:58 +0100

    after class activity completed

---
+++ script.js
@@ -0,0 +1,15 @@
+
+var x = 100;
+var y = 100;
+var z = x + y;
+document.getElementById("add").innerHTML = z;
+
+var x = 150;
+var y = 100;
+var z = x - y;
+document.getElementById("difference").innerHTML = z;
+
+var x = 120;
+var y = 55.5;
+var z = x * y;
+document.getElementById("quotient").innerHTML = z;
